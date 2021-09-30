9d37d088b6975db19d2162b508f5a05b80d37d45
K4pocha <k4pocha@gmail.com>
2021-09-30 07:11:01 -0300

    Funcion modificar evento

---
+++ Codigo/ActivitiesCalendar/src/main/java/com/mycompany/activitiescalendar/JavaCalendarAplication.java
@@ -0,0 +1,11 @@
+package com.mycompany.activitiescalendar;
+
+/**
+ *
+ * @author k4po
+ */
+public class JavaCalendarAplication {
+    public static void main(String[] args) {
+        Menu.menu();
+    }
+}
